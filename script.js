c6135911a6e9996a61d28f0050d21474e30554f0
LisaT2005 <lisathayil@gmail.com>
2022-06-25 15:04:41 -0500

    hi

---
+++ script.js
@@ -0,0 +1,20 @@
+/*const square = document.createElement("div");
+square.appendChild(document.getElementsByClassName("square"));
+const element = document.getElementsByClassName("squares");
+
+element.appendChild(square);*/
+
+var original = document.getElementById("squareID");
+var clone = original.cloneNode(true);
+clone.removeAttribute("id");
+document.getElementById("squareID").appendChild(clone);
+
+function createSquare(){
+    const div = document.createElement("div");
+    div.setAttribute("class", "square");
+   
+    const currentDiv = document.getElementById("squareID");
+    document.body.insertBefore(newDiv, div);
+
+    
+}
