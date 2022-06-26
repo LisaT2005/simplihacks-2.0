38ce765ef36d8eba2efd2f9c51d8d6573ea0a89a
LisaT2005 <lisathayil@gmail.com>
2022-06-26 03:14:59 -0500

    updated recipes, image upload, etc

--- recipesAddScript.js
+++
@@ -1,3 +0,0 @@
-function newElement(){
-    recipesScript.newElement();
-}
--- recipesScript.js
+++ recipesScript.js
@@ -1,29 +1,46 @@
 
      
     
+    let image;
+    
+ 
+    
     
     const basicButton = document.createElement("button")
      
+     ,  img = document.createElement('img')
      ,  title = document.createElement('p')
-     ,  credit = document.createElement('p')
-     ,  img = document.createElement('img');
+     ,  credit = document.createElement('p') 
      ;
 
      basicButton.className = "square";
-      basicButton.setAttribute("style", "/recipeStyle.css");
+      basicButton.setAttribute("style", "/style.css");
 
       img.className = 'recipe-image';
-      img.setAttribute("style", "/recipeStyle.css");
+      img.setAttribute("style", "/style.css");
     
      title.className = "recipe-title";
-     title.setAttribute("style", "/recipeStyle.css");
+     title.setAttribute("style", "/style.css");
      
      credit.className = "recipe-credit";
-     credit.setAttribute("style", "/recipeStyle.css");
+     credit.setAttribute("style", "/style.css");
 
+     var loadFile = function(event) {
+      image = document.getElementById('output');
+      image.src = URL.createObjectURL(event.target.files[0]);
+      img.className = 'recipe-image';
+      img.setAttribute("style", "/style.css");
+      img.src = URL.createObjectURL(event.target.files[0]);
+      basicButton.appendChild(img);
+      basicButton.appendChild(title);
+     basicButton.appendChild(credit);
+  };
      
+     basicButton.appendChild(img);
      basicButton.appendChild(title);
      basicButton.appendChild(credit);
+
+    
      
      // Create a new list item when clicking on the "Add" button
      function newElement() {
@@ -36,10 +53,13 @@
        
        var titleInput = document.getElementById("titleInput").value;
        let creditInput = document.getElementById("creditInput").value;
+       let imgInput = document.getElementById("myFile");
       
        
-        li.childNodes[0].textContent = titleInput;
-        li.childNodes[1].textContent = creditInput;
+        li.childNodes[0] = imgInput;
+       li.childNodes[1].textContent = titleInput;
+        li.childNodes[2].textContent = creditInput;
+
 
         li.addEventListener("click", function(){
             window.open(document.getElementById("linkInput").value);
@@ -54,7 +74,7 @@
        
        
 
-       if (titleInput === '' || creditInput === '') {
+       if (titleInput === '' || creditInput === ''|| linkInput === '') {
          alert("You must fill in all blanks!");
        } else {
          document.getElementById("myUL").appendChild(li);
@@ -73,3 +93,4 @@
          }
        }
      }
+
