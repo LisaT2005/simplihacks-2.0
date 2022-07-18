2ba2b696e7ec6e69fffcc24c9a730884b7a2d664
LisaT2005 <lisathayil@gmail.com>
2022-07-18 11:01:35 -0500

    UI changes

---
+++ form.js
@@ -0,0 +1,15 @@
+//this code doesn't do anything  right now
+//the plan is to use this script to transfer data between the form and calendar
+//but idk how to do that yet, still researching lol
+
+//let month;
+let day;
+//let date;
+
+function getDate(inputDay){
+    const day = inputDay;
+    return day;
+
+}
+
+//document.getElementById('results').append();
